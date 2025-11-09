1374064a33e2e31a977f7de1eaf62dfb3bc2a648
Salwalabibah <salwalabibah04@gmail.com>
2025-11-09 16:09:40 +0700

    to-do's created api

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('to-do-list', [ToDoController::class, 'index'])->name('to-do.index');
-Route::post('to-do-list/store', [ToDoController::class, 'store'])->name('to-do.store');
